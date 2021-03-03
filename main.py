576e73395c9ad49c20c6cd1ee31e66c667f611c5
jongyun lee <lovgrammer@gmail.com>
2021-03-03 16:35:46 +0900

    updated

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import re
 
-data = pd.read_csv("data.txt", header=None, delimiter=r"\s+")
+data = pd.read_csv("20180618/tcpprobe4.log", header=None, delimiter=r"\s+")
 ndata = pd.DataFrame(columns="time acked".split())
 ndata['time'] = data[0]
+ndata['port'] = data[2]
 ndata["acked"] = data[4]
 ndata = ndata.iloc[0:-1]
 
-ndata["time"] = ndata["time"].astype(float)
-ndata.loc[:,"time"] *= 1000
-ndata["time"] = ndata["time"].astype(int)
-ndata['acked'] = ndata.acked.apply(lambda x: int(x[2:-2], 16))
+#ndata["acked"] = ndata["acked"].astype(str).str[2:-2]
+ndata["port"] = ndata["port"].astype(str).str[-5:]
+#print(ndata["port"])
 
-ndata = ndata.iloc[10:-3000]
+ndata["time"] = ndata["time"].astype(float)
+ndata["time"] = ndata["time"] - ndata.iloc[0]["time"]
+
+#ndata['acked'] = ndata.acked.apply(lambda x: int(x[2:-2], 16))
+ndata['acked'] = ndata.acked.apply(lambda x: int(x[2:], 16))
+
+ndata = ndata[ndata['port'].astype(str) == "39935"]
+#print(ndata["acked"])
+
 
 # plt.plot(ndata["time"], ndata["acked"])
 # plt.show()
@@ -24,38 +33,43 @@ cnt = 0
 
 repeats = []
 times = []
+itv = []
 for idx, row in ndata.iterrows():
     cnt = cnt + 1
     # if cnt == 100:
     #     break
     if idx < len(ndata) -1:
-        pkt_cnt = (ndata.iloc[idx+1]["acked"] - row["acked"]) / 1388
-        # print(pkt_cnt)
-        if pkt_cnt > 0:
-            times.append(ndata.iloc[idx+1]["time"] - row["time"])
-            repeats.append(pkt_cnt)
+        pkt_cnt = (ndata.iloc[idx+1]["acked"] - row["acked"]) / 1350.0
+        tv = ndata.iloc[idx+1]["time"] - row["time"]
+        times.append(tv)
+        if pkt_cnt <= 0:
+              repeats.append(0)
+              itv.append(0)
+        else:
+              repeats.append(pkt_cnt)
+              itv.append(tv/pkt_cnt)
         # for i in range(int(pkt_cnt)):
         #     nndata.loc[len(nndata)] = [row["time"]]
         
 
+
+
 what = []
 ntimes = np.array(what)
-itv = np.array(times)/np.array(repeats)
-# print(itv)
+
 for i in range(len(itv)):
-    if ndata.iloc[i]["time"] == ndata.iloc[i+1]["time"]:
         ntimes = np.append(ntimes, np.arange(repeats[i]) * itv[i], axis=0)
-    else:
-        ntimes = np.append(ntimes, np.arange(repeats[i]) * 0.0, axis=0)
 
     
 ndata = ndata[0:len(repeats)]
-print(ndata.index.repeat(repeats))
 ndata = ndata.loc[ndata.index.repeat(repeats)].reset_index(drop=True)
 
-
+print(len(ndata))
+print(len(ntimes))
     
 ndata.loc[:,"time"] += ntimes
+ndata.loc[:,"time"] *= 1000
+ndata["time"] = ndata["time"].astype(int)
 ndata = ndata["time"]
 ndata.to_csv("output.txt", header=None, index=False)
 
